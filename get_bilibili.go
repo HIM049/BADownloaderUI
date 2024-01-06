31bf534736d075f5c4e5fb25154da5a2fff08d15
HIM049 <him_049@163.com>
2024-01-06 19:52:47 +0800

    添加登录请求部分代码

--- get_bilibili.go
+++ get_bilibili.go
@@ -10,6 +10,116 @@ import (
 	"strconv"
 )
 
+// 用于获取登录 KEY 和 QR code 的函数
+type GetLoginKeyReturn struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    struct {
+		Url        string `json:"url"`
+		Qrcode_key string `json:"qrcode_key"`
+	}
+}
+
+func getLoginKey() (string, error) {
+	resp, err := http.Get("https://passport.bilibili.com/x/passport-login/web/qrcode/generate")
+	if err != nil {
+		return "", err
+	}
+	// 将 body 转为字符串并返回
+	body, _ := io.ReadAll(resp.Body)
+	bodyString := string(body)
+	defer resp.Body.Close()
+	return bodyString, nil
+}
+func GetLoginKey() (string, string, error) {
+	var obj GetLoginKeyReturn
+	body, err := getLoginKey()
+	if err != nil {
+		return "", "", err
+	}
+	err = decodeJson(body, &obj)
+	if err != nil {
+		return "", "", err
+	}
+	// 错误检查
+	if checkObj(obj.Code) {
+		return "", "", errors.New(obj.Message)
+	}
+	return obj.Data.Url, obj.Data.Qrcode_key, nil
+}
+
+// 用于检查扫码状态和获取 cookie 的函数
+type checkLoginReturn struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    struct {
+		Url           string `json:"url"`           // 游戏分站跨域登录 url
+		Refresh_token string `json:"refresh_token"` // 刷新 refresh_token
+		Timestamp     int    `json:"timestamp"`     // 登录时间
+		Code          int    `json:"code"`          // 0：扫码登录成功 86038：二维码已失效 86090：二维码已扫码未确认 86101：未扫码
+		Message       string `json:"message"`       // 扫码状态信息
+	}
+}
+
+// 定义一个带有参数和cookie的get请求函数，返回响应和错误
+func checkLoginStatus(qrcode_key string) (string, *[]*http.Cookie, error) {
+	// 创建一个 HTTP 客户端
+	client := &http.Client{}
+
+	// 创建一个 GET 请求
+	req, err := http.NewRequest("GET", "https://passport.bilibili.com/x/passport-login/web/qrcode/poll", nil)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// 添加参数到请求的查询字符串
+	q := req.URL.Query()
+	q.Add("qrcode_key", qrcode_key)
+	req.URL.RawQuery = q.Encode()
+
+	// 发送请求并获取响应
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", nil, err
+	}
+	defer resp.Body.Close()
+
+	// 检查响应状态码
+	if resp.StatusCode != http.StatusOK {
+		return "", nil, errors.New("Error:" + strconv.Itoa(resp.StatusCode))
+	}
+
+	// 读取 Set-Cookie 头部信息
+	cookies := resp.Cookies()
+	// for _, cookie := range cookies {
+	// 	fmt.Println("Cookie:", cookie.Name, "=", cookie.Value)
+	// }
+
+	// 将 body 转为字符串并返回
+	body, _ := io.ReadAll(resp.Body)
+	bodyString := string(body)
+	defer resp.Body.Close()
+	return bodyString, &cookies, nil
+}
+
+func CheckLoginStatus(qrcode_key string) (*checkLoginReturn, *[]*http.Cookie, error) {
+	var obj checkLoginReturn
+	body, cookies, err := checkLoginStatus(qrcode_key)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = decodeJson(body, &obj)
+	if err != nil {
+		return nil, nil, err
+	}
+	// 错误检查
+	if checkObj(obj.Code) {
+		return nil, nil, errors.New(obj.Message)
+	}
+
+	return &obj, cookies, nil
+}
+
 // 用于获取收藏夹基本信息的函数
 // 传入收藏夹 ID ，ps 单页大小， pn 页码
 // 获得如下结构体
